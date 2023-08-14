7ddbc35d11e722f7a2bf0faa0fa492216adf9f87
adarsh-francraze <adarsh@fancraze.com>
2023-08-14 14:49:18 +0530

    Added backend

---
+++ backend/utils/db.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "dbuser",
+  password: "s3kreee7",
+  database: "my_db",
+});
+
+connection.connect();
+
+module.exports = connection;
